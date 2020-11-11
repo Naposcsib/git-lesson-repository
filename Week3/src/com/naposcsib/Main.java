9aa4f2c089c5afd900fabb979a044e17a2438496
Naposcsib <jahnbeata14@gmail.com>
2020-11-11 17:48:14 +0100

    Main ready

---
+++ Week3/src/com/naposcsib/Main.java
@@ -0,0 +1,16 @@
+package com.naposcsib;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Java alapok");
+	System.out.println("Primitív változók: byte short int long float double boolean char");
+	System.out.println("Referencia típusú változók");
+	//Ez itt egy egysoros komment
+	int i = 8;
+	int j = 7;
+	int k = i + j;
+	System.out.println(k + " az összeg");
+
+    }
+}
