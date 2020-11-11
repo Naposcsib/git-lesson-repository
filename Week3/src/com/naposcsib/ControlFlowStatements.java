e69048554b3acb0ec1c5f7970460397a471be64a
Naposcsib <jahnbeata14@gmail.com>
2020-11-11 18:06:45 +0100

    Main, LogiClass, ControlFlowStatements added

---
+++ Week3/src/com/naposcsib/ControlFlowStatements.java
@@ -0,0 +1,7 @@
+package com.naposcsib;
+
+public class ControlFlowStatements {
+    public static void main(String[] args){
+        //Ide jön majd az if, if-else, else
+    }
+}
---
+++ Week3/src/com/naposcsib/LogiClass.java
@@ -0,0 +1,11 @@
+package com.naposcsib;
+
+public class LogiClass {
+    public static void main(String[] args){
+        System.out.println("Operátorok: ");
+        System.out.println("- Értékadó: +=, -=, *=, /=, %=");
+        System.out.println("- Reláció: < > <= >= ");
+        System.out.println("- Hármas feltételes: ? : ");
+        System.out.println("- Logikai operátorok: && || ");
+    }
+}
