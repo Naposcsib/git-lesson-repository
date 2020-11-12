2d87053d9955ee9ed230e8a147542b897c380255
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 12:33:33 +0100

    IntroduceYourself added

--- Week3/src/com/naposcsib/HelloOthers.java
+++ Week3/src/com/naposcsib/HelloOthers.java
@@ -2,11 +2,7 @@ package com.naposcsib;
 
 public class HelloOthers {
     public static void main(String[] args) {
-        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
-        //
-        // Helló Eszter!
-        // Helló Maris!
-        // Helló Józsi!
+
         System.out.println("Helló Eszter!");
         System.out.println("Helló Maris!");
         System.out.println("Helló Józsi!");
---
+++ Week3/src/com/naposcsib/IntroduceYourself.java
@@ -0,0 +1,11 @@
+package com.naposcsib;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+
+        System.out.println("Jáhn Beáta");
+        System.out.println(36);
+        System.out.print(163.5);
+
+    }
+}
