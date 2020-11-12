04f2bfda86f668fff0ab282add1c46f214bb485a
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 14:46:43 +0100

    DefineBasicInfo added

---
+++ Week3/src/com/naposcsib/DefineBasicInfo.java
@@ -0,0 +1,22 @@
+package com.naposcsib;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Beata Jahn";
+        System.out.println(name);
+
+        int age = 36;
+        System.out.println(age);
+
+        double height = 163.5;
+        System.out.println(height);
+
+        boolean married = true;
+        System.out.print(married);
+        // Hozz létre pár dolgot változóként és utána írasd ki az értékeiket:
+        // - A neved karakterláncként (string)
+        // - Korod egész számként (integer)
+        // - Magasságod méterben törtszámként (float / double)
+        // - Házas vagy-e logikai értékként (boolean)
+    }
+}
