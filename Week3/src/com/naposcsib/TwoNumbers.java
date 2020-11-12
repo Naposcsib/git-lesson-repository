74f9105a46dbeb549d699f722821b07efdedfaf4
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 13:58:28 +0100

    TwoNumbers added

---
+++ Week3/src/com/naposcsib/TwoNumbers.java
@@ -0,0 +1,16 @@
+package com.naposcsib;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int i = 13;
+        int j = 22;
+
+        System.out.println(i + j);
+        System.out.println(i - j);
+        System.out.println(i * j);
+        System.out.println(13 / 22);
+        System.out.println(13. / 22.);
+        System.out.println(i % j);
+
+    }
+}
