8fe1a8ead8cb3110ae90edbc1bb3394a67e1e8cf
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 14:25:58 +0100

    FavouriteNumber added

---
+++ Week3/src/com/naposcsib/FavoriteNumber.java
@@ -0,0 +1,10 @@
+package com.naposcsib;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 7;
+
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+
+    }
+}
