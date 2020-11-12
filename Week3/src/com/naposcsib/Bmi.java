136dae673caeaa9229ef5e52372cf6167d810f61
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 14:37:14 +0100

    Bmi added

---
+++ Week3/src/com/naposcsib/Bmi.java
@@ -0,0 +1,14 @@
+package com.naposcsib;
+
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        double bmi = massInKg / (heightInM * heightInM);
+        System.out.println(bmi);
+
+        // Számíttasd és írasd ki a testtömeg-indexet (BMI)
+        // a változókat használva
+    }
+}
