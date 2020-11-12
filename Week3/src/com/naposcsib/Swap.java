61ad5975bc4219673f00e61efd807a8a0627ce12
Naposcsib <jahnbeata14@gmail.com>
2020-11-12 14:31:49 +0100

    Swap added

---
+++ Week3/src/com/naposcsib/Swap.java
@@ -0,0 +1,17 @@
+package com.naposcsib;
+
+public class Swap {
+    public static void main(String[] args) {
+        // Cseréld meg a 2 változó értékét programozás útján.
+        // Az a értéke legyen az, ami a b változóban van és fordítva.
+        int a = 123;
+        a = 526;
+
+        int b = 526;
+        b = 123;
+
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
