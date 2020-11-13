50fb547dff0c894f6f2cbe2a48cee5d86bcbabdb
Naposcsib <jahnbeata14@gmail.com>
2020-11-13 05:17:47 +0100

    MultiplicationTable added

---
+++ Week3/src/com/naposcsib/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package com.naposcsib;
+
+import java.util.Scanner;
+import java.util.Scanner;
+public class MultiplicationTable {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Írj be egy egész számot!");
+        int number = scanner.nextInt();
+
+        for (int a = 1; a <= 10; a++){
+            System.out.println(a + " * " + number + " = " + number * a);
+        }
+
+    }
+}
