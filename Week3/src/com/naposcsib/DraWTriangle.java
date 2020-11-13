02edd31161766bbf4885584e737b48bb5ec63e59
Naposcsib <jahnbeata14@gmail.com>
2020-11-13 12:39:35 +0100

    DraWTriangle added

--- Week3/src/com/naposcsib/DraWTriangle.java
+++ Week3/src/com/naposcsib/DraWTriangle.java
@@ -5,21 +5,16 @@ public class DraWTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kérek egy egész számot!");
         int lineCount = scanner.nextInt();
-        int a = 1;
-        while (a <= lineCount){
+
+        for (int b = 0; b < lineCount; b++) {
+            for (int a = 0; a < b; a++) {
+
+                System.out.print("* ");
+            }
             System.out.println("*");
-            a++;
         }
+
+
     }
 }
-// Hozz létre egy egész szám típusú, lineCount nevű változót.
-// Írj egy programot, ami egy számot kér a standard input-ból és
-// elmenti azt a lineCount változóban.
-// Rajzoljon egy ilyen háromszöget:
-//
-// *
-// **
-// ***
-// ****
-//
-// A háromszögnek annyi sora legyen, mint amennyi a lineCount értéke.
+
