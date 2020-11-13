7f77a4d1ea584b5d8ef22a3930d072022449305c
Naposcsib <jahnbeata14@gmail.com>
2020-11-13 08:38:50 +0100

    FizzBuzz added

--- Week3/src/com/naposcsib/FizzBuzz.java
+++ Week3/src/com/naposcsib/FizzBuzz.java
@@ -3,11 +3,19 @@ package com.naposcsib;
 public class FizzBuzz {
     public static void main(String[] args) {
         int a = 1;
-        while (a)
-        // Írj egy programot, ami 1-től 100-ig kiíratja a számokat.
-// De a 3 többszörösei esetén a "Fizz" szót írja ki a szám helyett
-// és az 5 többszöröseinél pedig a "Buzz" szót írja ki a szám helyett.
-// Ha a szám a 3-nak és 5-nek is többszöröse,
-// akkor a "FizzBuzz" szót írja ki a szám helyett
+        while (a <= 100) {
+            a++;
+            if (a % 3 == 0 && a % 5 == 0) {
+            System.out.println("FizzBuzz");
+            }
+            else if (a % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (a % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(a);
+            }
+        }
     }
 }
+
