3253b18face4da69aa93c87ef47674a7de351931
Naposcsib <jahnbeata14@gmail.com>
2020-11-13 02:45:16 +0100

    HelloUser added

---
+++ Week3/src/com/naposcsib/HelloUser.java
@@ -0,0 +1,13 @@
+package com.naposcsib;
+import java.util.Scanner;
+public class HelloUser {
+    public static void main(String[] args) {
+
+        System.out.println("Írd be a neved!");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput1 = scanner.nextLine();
+
+        System.out.println("Hello," + userInput1 + "!");
+    }
+}
