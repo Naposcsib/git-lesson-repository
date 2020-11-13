666714eed748f290c2c597e363f03a56fb746d0e
Naposcsib <jahnbeata14@gmail.com>
2020-11-13 02:38:08 +0100

    SecondsInADay added

--- Week3/src/com/naposcsib/Cuboid.java
+++ Week3/src/com/naposcsib/Cuboid.java
@@ -8,14 +8,10 @@ public class Cuboid {
         double c = 3;
 
         double f = 2 * (a * b + b * c + c * a);
-        double t = a * a * a;
+        double t = a * b * c;
 
         System.out.println("Felület: " + f);
         System.out.println("Térfogat: " + t);
-        // Írj egy programot, ami egy téglatest 3 oldalát tartalmazza változókban (doubles)
-        // A program írja ki a felületét és térfogatát a testnek ilyen formátumban:
-        //
-        // Felület: 600
-        // Térfogat: 1000
+
     }
 }
---
+++ Week3/src/com/naposcsib/SecondsInADay.java
@@ -0,0 +1,14 @@
+package com.naposcsib;
+
+public class SecondsInADay {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        int seconds = (23 - currentHours) * 3600 + (59 - currentMinutes) * 60 + (60 - currentSeconds);
+        System.out.println(seconds + " másodperc van hátra a napból.");
+
+
+    }
+}
