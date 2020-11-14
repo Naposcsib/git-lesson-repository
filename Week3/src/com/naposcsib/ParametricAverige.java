0e3f43232259b68e3dee4a4ecd1b269cd5ac7146
Naposcsib <jahnbeata14@gmail.com>
2020-11-14 12:52:30 +0100

    ParametricAverige half added

---
+++ Week3/src/com/naposcsib/ParametricAverige.java
@@ -0,0 +1,19 @@
+package com.naposcsib;
+import java.util.Scanner;
+public class ParametricAverige {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hány szám átlagát szeretnég kiszámítani?");
+        int numberOfNumbers = scanner.nextInt();
+        System.out.println("Írj be" + numberOfNumbers +"számot!");
+        int number
+
+    }
+}
+// Írj egy programot, ami egy számot kér a standard input-ból.
+// Amennyi az első szám értéke volt, annyiszor kell új számot kérnie.
+// Ha megkapta az összes számot, printelje ki az összegét és
+// az átlagát ezeknek a számoknak a következő formátumban:
+//
+// Összeg: 22, Átlag: 4.4
