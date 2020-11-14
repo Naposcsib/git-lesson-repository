9366ea56766b7e821ceca3a9ca1ee9c98e68c28c
Naposcsib <jahnbeata14@gmail.com>
2020-11-14 15:11:27 +0100

    ParametricAverage added

--- Week3/src/com/naposcsib/ParametricAverige.java
+++ Week3/src/com/naposcsib/ParametricAverige.java
@@ -4,11 +4,22 @@ public class ParametricAverige {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hány szám átlagát szeretnég kiszámítani?");
+        System.out.println("Hány szám átlagát szeretnéd kiszámítani?");
         int numberOfNumbers = scanner.nextInt();
-        System.out.println("Írj be" + numberOfNumbers +"számot!");
-        int number
+        System.out.println("Írj be " + numberOfNumbers + " számot!");
+        float collector = 0;
+        for (int i = 1; i <= numberOfNumbers; i++) {
+            System.out.println("Szám " + i + " :");
+            float number = scanner.nextFloat();
+            collector += number;
+            System.out.print("Összeg: ");
+            System.out.println(collector + " ");
+            if (i == numberOfNumbers) {
+                System.out.print("Átlag: ");
+                System.out.println(collector / numberOfNumbers);
+            }
 
+        }
     }
 }
 // Írj egy programot, ami egy számot kér a standard input-ból.
