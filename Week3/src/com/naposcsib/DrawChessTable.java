cc0fe1d55a03d2b3f510987f35e95af5399241f1
Naposcsib <jahnbeata14@gmail.com>
2020-11-14 15:55:48 +0100

    DrawChessTable added

---
+++ Week3/src/com/naposcsib/DrawChessTable.java
@@ -0,0 +1,35 @@
+package com.naposcsib;
+import java.util.Scanner;
+
+public class DrawChessTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Írj be egy karaktert!");
+        String chessCube = scanner.next();
+        System.out.print(" ");
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(chessCube);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+                System.out.println(" ");
+            if (i % 2 == 0){
+                System.out.print(" ");
+            }
+        }
+    }
+}
+// Írj egy programot, ami egy ilyen "sakktáblát" rajzol ki:
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
