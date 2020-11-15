e75c0a022e9a28ea7471a276af6b541c10f62848
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 10:33:55 +0100

    MultiplicationOfElements added

---
+++ Week3/src/com/naposcsib/MultiplicationOfElements.java
@@ -0,0 +1,15 @@
+package com.naposcsib;
+
+public class MultiplicationOfElements {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[4];
+        numbers[0] = 54;
+        numbers[1] = 23;
+        numbers[2] = 66;
+        numbers[3] = 12;
+
+       System.out.println(numbers[2] + numbers[3]);
+
+    }
+}
