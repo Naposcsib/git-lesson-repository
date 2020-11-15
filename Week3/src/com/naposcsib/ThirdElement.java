61f3fd11d13f95240e3aef320d65672edcbe07ac
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 10:05:09 +0100

    ThirdElement added

---
+++ Week3/src/com/naposcsib/ThirdElement.java
@@ -0,0 +1,13 @@
+package com.naposcsib;
+
+public class ThirdElement {
+    public static void main(String[] args) {
+        int [] numbers = new int [4];
+        numbers [0] = 4;
+        numbers [1] = 5;
+        numbers [2] = 6;
+        numbers [3] = 7;
+        System.out.println(numbers[2]);
+
+    }
+}
