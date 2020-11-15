3f5186dcb8ca14319e23ee3a5cb460e806070ec7
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 23:33:13 +0100

    Methods started

--- Week3/Functions/src/com/naposcsib/Doubling.java
+++ Week3/Functions/src/com/naposcsib/Doubling.java
@@ -11,9 +11,3 @@ public static int doubling (int a){
  }
 }
 
-// - Hozz létre egy `baseNum` nevű integer változót, aminek az értéke legyen `123`.
-
-// - Készíts egy függvényt `doubling` névvel, mely megduplázza a függvény
-//   bemeneti paraméterét és integer-ként adja vissza(return).
-
-// - Írasd ki a visszatérési értékét a `doubling(baseNum)` függvénynek.
