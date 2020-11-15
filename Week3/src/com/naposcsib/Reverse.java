6cf8fc623de3a5cea09c52a8bfc40442989b3d48
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 16:24:40 +0100

    Reverse added

---
+++ Week3/src/com/naposcsib/Reverse.java
@@ -0,0 +1,27 @@
+package com.naposcsib;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int[] numbers = new int[4];
+        numbers[0] = 4;
+        numbers[1] = 5;
+        numbers[2] = 6;
+        numbers[3] = 7;
+
+        int[] swap = new int[4];
+        for (int i = 0; i < numbers.length; i++){
+            swap[i] = numbers[i];
+            }
+            for (int j = 0; j < numbers.length; j++) {
+                numbers[j] = swap[numbers.length - 1 - j];
+                System.out.println(numbers[j]);
+            }
+        }
+    }
+
+    /*int[] swap = new int[4];
+        System.arraycopy(numbers, 0, swap, 0, numbers.length);
+                for (int j = 0; j < numbers.length; j++) {
+        numbers[j] = swap[numbers.length - 1 - j];
+        System.out.println(numbers[j]);
+        }*/
