42e16cdc215cdfe42d0d4cef3152e9c2fcb4b084
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 10:46:52 +0100

    ChangeElement added

---
+++ Week3/src/com/naposcsib/ChangeElement.java
@@ -0,0 +1,21 @@
+package com.naposcsib;
+
+public class ChangeElement {
+    public static void main(String[] args) {
+        int[] numbers = new int[6];
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 8;
+        numbers[4] = 5;
+        numbers[5] = 6;
+
+        numbers[3] =4;
+
+        System.out.println(numbers[3]);
+
+
+
+    }
+}
