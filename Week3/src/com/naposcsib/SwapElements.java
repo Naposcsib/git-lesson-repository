4334d59fe2ada44b4c82b2412f4c63d356eb1eca
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 15:47:53 +0100

    SwapElements added

---
+++ Week3/src/com/naposcsib/SwapElements.java
@@ -0,0 +1,23 @@
+package com.naposcsib;
+
+import java.util.Arrays;
+
+public class SwapElements {
+    public static void main(String[] args) {
+        String[] orders = new String[3];
+        orders[0] = "first";
+        orders[1] = "second";
+        orders[2] = "third";
+
+        System.out.println(Arrays.toString(orders));
+
+        String swap = orders[0];
+        orders[0] = orders[2];
+        orders[2] = swap;
+
+        System.out.println(Arrays.toString(orders));
+    }
+}
+// - Készíts egy `orders` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `["first", "second", "third"]`
+// - Cseréld fel az első és a harmadik elemet az `orders` tömbben
