72324b88479d5fa5bb9f6f54b1fe2ad78df6e8e7
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 10:54:27 +0100

    IncrementElement added

---
+++ Week3/src/com/naposcsib/IncrementElement.java
@@ -0,0 +1,18 @@
+package com.naposcsib;
+
+public class IncrementElement {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+
+        numbers[2] += 1;
+
+        System.out.println(numbers[2]);
+
+
+    }
+}
