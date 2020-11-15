973f309e2fd68f4ebed60ec4ea1e5edb0485dd53
Naposcsib <jahnbeata14@gmail.com>
2020-11-15 16:00:45 +0100

    SumAll added

---
+++ Week3/src/com/naposcsib/SumAll.java
@@ -0,0 +1,22 @@
+package com.naposcsib;
+
+public class SumAll {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        numbers[1] = 4;
+        numbers[2] = 5;
+        numbers[3] = 6;
+        numbers[4] = 7;
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            if (i == numbers.length -1) {
+
+                System.out.println("A tömb elemeinek összege: " + sum);
+            }
+        }
+    }
+}
