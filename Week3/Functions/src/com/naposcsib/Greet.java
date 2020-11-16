a39c87e0024397a2bc5b0376b74a063eb088d845
Naposcsib <jahnbeata14@gmail.com>
2020-11-16 08:43:27 +0100

    Greet added

---
+++ Week3/Functions/src/com/naposcsib/Greet.java
@@ -0,0 +1,14 @@
+package com.naposcsib;
+
+public class Greet {
+
+    public static void main(String[] args) {
+        String name = "Green Fox";
+        greet(name);
+    }
+
+    public static void greet(String text) {
+        System.out.println("Üdv, kedves " + text + " !");
+    }
+}
+
