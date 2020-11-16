454810f4644d85d7248fc6ef9a4c5ea3c0b96b43
Naposcsib <jahnbeata14@gmail.com>
2020-11-16 09:38:49 +0100

    AppendAFunc Added

--- Week3/Functions/src/com/naposcsib/AppendAFunc.java
+++ Week3/Functions/src/com/naposcsib/AppendAFunc.java
@@ -11,10 +11,3 @@ public class AppendAFunc {
         return text + "a";
     }
 }
-// - Hozz létre egy class-t `AppendAFunc` névvel és benne a main függvényt.
-
-// - Hozz létre egy `typo` nevű String változót, aminek az értéke legyen `Chinchill`.
-// - Készíts egy függvényt `appendAFunc` névvel mely hozzácsatol egy 'a'
-//   karaktert a függvény bemeneti paraméteréhez és String-ként adja vissza(return).
-
-// - Írasd ki a visszatérési értékét a `appendAFunc(typo)` függvénynek.
