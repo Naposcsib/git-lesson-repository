d09040244a0d0b9bc8af8322edccda3e038b8454
Naposcsib <jahnbeata14@gmail.com>
2020-11-19 12:15:42 +0100

    UrlFixer added

--- Week4/src/com/naposcsib/UrlFixer.java
+++ Week4/src/com/naposcsib/UrlFixer.java
@@ -3,6 +3,7 @@ package com.naposcsib;
 public class UrlFixer {
     public static void main(String... args) {
         String url = "https//www.reddit.com/r/nevertellmethebots";
-        System.out.println((url.replaceFirst("/", ":/") + url.replace("bots", "odds") + "/"));
+
+        System.out.println(url.replace("bots", "odds").replaceFirst("/", ":/") + "/");
     }
 }
